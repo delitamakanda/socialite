5a9af74ade38e7534fbb9829f4142d37dac3a138
Délita Makanda <delitamakanda@users.noreply.github.com>
2017-02-23 13:33:41 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return '<h1>app work!</h1>'
+	user_agent = request.headers.get('User-Agent')
+	return '<h1>app work! Your browser is %s</h1>' % user_agent
 
 @app.route('/user/<name>')
 def user(name):
