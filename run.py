13ec91007fdcaf3e99a7fd74b7cb59308ca30bdd
Délita Makanda <delitamakanda@users.noreply.github.com>
2017-02-23 14:05:52 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, session, redirect, url_for
+from flask import Flask, request, render_template, session, redirect, url_for, flash
 app = Flask(__name__)
 
 @app.route('/')
