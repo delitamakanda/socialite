e0086a2341c7b39d9933910b2aae74dadd5049ca
Délita Makanda <delitamakanda@users.noreply.github.com>
2017-02-23 13:42:22 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,17 +1,19 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
 	user_agent = request.headers.get('User-Agent')
-	return '<h1>app work! Your browser is %s</h1>' % user_agent
+	#return '<h1>app work! Your browser is %s</h1>' % user_agent
+	return render_template('index.html')
 
 @app.route('/user/<name>')
 def user(name):
 	user = load_user(name):
 	if not user:
 		abort(404)
-	return '<h1>app work %s!</h1>' % name
+	#return '<h1>app work %s!</h1>' % name
+	return render_template('user.html', name=name)
 
 if __name__ == '__main__':
 	app.run(debug=True)
