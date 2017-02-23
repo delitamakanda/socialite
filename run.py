e235e889ef989f1de3ac4582957ddbb36861e2eb
Délita Makanda <delitamakanda@users.noreply.github.com>
2017-02-23 13:56:08 +0100

    Update run.py

--- run.py
+++ run.py
@@ -15,5 +15,13 @@ def user(name):
 	#return '<h1>app work %s!</h1>' % name
 	return render_template('user.html', name=name)
 
+@app.errorhandler(400):
+def page_not_found(e):
+	return render_template('404.html'), 404
+
+@app.errorhandler(500):
+def internal_server_error(e):
+	return render_template('500.html'), 500
+
 if __name__ == '__main__':
 	app.run(debug=True)
