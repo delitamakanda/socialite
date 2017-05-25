533e7a660c1e766d48f4eb8c6d8cc3bb860e1197
Délita Makanda <delita.makanda@gmail.com>
2017-05-26 01:50:15 +0200

    add more files

--- app/auth/views.py
+++ app/auth/views.py
@@ -61,7 +61,7 @@ def before_request():
 
 @auth.route('/unconfirmed')
 def unconfirmed():
-    if current_user.is_anonymous() or current_user.confirmed:
+    if current_user.is_anonymous or current_user.confirmed:
         return redirect('main.index')
     return render_template('auth/unconfirmed.html')
 
