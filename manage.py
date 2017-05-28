2f82ff5818b309178b395de341ff35b1ef6f1ced
Délita Makanda <delita.makanda@gmail.com>
2017-05-29 01:04:26 +0200

    change variables

--- manage.py
+++ manage.py
@@ -10,7 +10,7 @@ from app.models import User, Role, Post, Permission, Follow, Comment
 from flask.ext.script import Manager, Shell
 from flask.ext.migrate import Migrate, MigrateCommand
 
-app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+app = create_app(os.getenv('CONFIG') or 'default')
 manager = Manager(app)
 migrate = Migrate(app, db)
 
