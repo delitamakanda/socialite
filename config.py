bd7f7035928dcd0b9de52ab0540a45e447ae34d8
Délita Makanda <delita.makanda@gmail.com>
2017-05-29 00:15:41 +0200

    add mail handler for errors

--- config.py
+++ config.py
@@ -37,6 +37,28 @@ class TestingConfig(Config):
 class ProductionConfig(Config):
 	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 
+	@classmethod
+	def init_app(cls, app):
+		Config.init_app(app)
+
+		import logging
+		from logging.handlers import SMTPHandler
+		credentials = None
+		secure = None
+		if getattr(cls, 'MAIL_USERNAME', None) is not None:
+			credentials = (cls.MAIL_USERNAME, cls.MAIL_PASSWORD)
+			if getattr(cls, 'MAIL_USE_TLS', None):
+				secure = ()
+		mail_handler = SMTPHandler(
+			mailhost=(cls.MAIL_SERVER, cls.MAIL_PORT),
+			fromaddr=cls.MAIL_SENDER,
+			toaddrs=[cls.ADMIN],
+			subject=cs.MAIL_SUBJECT_PREFIX + ' Application error',
+			credentials=credentials,
+			secure=secure)
+		mail_handler.setLevel(logging.ERROR)
+		app.logger.addHandler(mail_handler)
+
 config = {
 	'development': DevelopmentConfig,
 	'testing': TestingConfig,
--- manage.py
+++ manage.py
@@ -20,10 +20,18 @@ manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 
 @manager.command
+def deploy():
+    from flask.ext.migrate import upgrade
+    from app.models import User, Role
+    upgrade()
+    Role.insert_roles()
+    User.add_self_follows()
+
 def profile(length=25, profile_dir=None):
     from werkzeug.contrib.profiler import ProfilerMiddleware
     app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[length], profile_dir=profile_dir)
     app.run()
+
 def test(coverage=False):
     if coverage and not os.environ.get('COVERAGE'):
         import sys
