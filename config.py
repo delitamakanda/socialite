ec58213a3b3f17aa8f89f13230b0ef86aa30d3e4
Délita Makanda <delita.makanda@gmail.com>
2017-05-29 03:15:59 +0200

    add variables

--- config.py
+++ config.py
@@ -3,7 +3,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
 	SECRET_KEY = 'dummy_secret_key'
-	SQLALCHEMY_COMMIT_ON_TEARDOWN = True
 	MAIL_SUBJECT_PREFIX = 'Socialite'
 	MAIL_SENDER = 'Socialite Team <team.socialite.app@gmail.com>'
 	ADMIN = 'delita.makanda@gmail.com',
@@ -12,7 +11,10 @@ class Config:
 	COMMENTS_PER_PAGE = 10
 	SLOW_DB_QUERY_TIME = 0.5
 	SQLALCHEMY_RECORD_QUERIES = True
-	SSL_DISABLE = True
+	SSL_DISABLE = False
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_RECORD_QUERIES = True
 	MAIL_SERVER = 'smtp.googlemail.com'
 	MAIL_PORT = 587
 	MAIL_USE_TLS = True
