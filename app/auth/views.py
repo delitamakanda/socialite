0bc800466de84675c33a1298329e4cfbac5b2306
Délita Makanda <delitamakanda@users.noreply.github.com>
2017-06-01 14:52:43 +0200

    Update views.py

--- app/auth/views.py
+++ app/auth/views.py
@@ -127,7 +127,7 @@ def password_reset(token):
             return redirect(url_for('auth.login'))
         else:
             return redirect(url_for('main.index'))
-    return render_template('auth/reset_password.html', form=form)
+    return render_template('auth/reset_password_form.html', form=form)
 
 
 @auth.route('/change-email', methods=['GET', 'POST'])
