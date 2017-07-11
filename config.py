29e7bcfa91a4cf3681c82f669a063089387d4119
Délita Makanda <delitamakanda@users.noreply.github.com>
2017-07-11 13:14:53 +0200

    add markdown syntax

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ class Config:
 	MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
 	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 	FLATPAGES_EXTENSION = '.md'
-	WHOOSH_BASE = '/ponywhoosh_indexes'
+	FLATPAGES_MARKDOWN_EXTENSIONS = ['codehilite', 'headerid']
 	ADMINS = ['delita.makanda@gmail.com', 'makanda.delita@orange.fr']
 
 
