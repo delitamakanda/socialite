642c54a4b6e1f712d499a7674dd839f68d55dc17
Délita Makanda <delitamakanda@users.noreply.github.com>
2017-07-26 11:01:15 +0200

    Update assets.py

--- app/util/assets.py
+++ app/util/assets.py
@@ -1,2 +1,15 @@
 from flask_assets import Bundle, Environment
+from flask import current_app
 
+bundles = {
+  'root_js': Bundle(
+    'js/lib/jquery-2.1.1.min.js',
+    'js/app.js',
+    output='dist/bundle.js'),
+  
+  'root_css': Bundle(
+    'css/lib/reset.css',
+    'css/common.css',
+    'css/app.css',
+    output='dist/styles.css')
+}
