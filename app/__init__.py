e0e92135e00a9bc90c5443476e1c2d4d2aa88d0d
Délita Makanda <delitamakanda@users.noreply.github.com>
2017-08-01 14:13:29 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ from flask.ext.pagedown import PageDown
 from flask.ext.flatpages import FlatPages
 from config import config
 
-from .util import assets
+from util import assets
 
 mail = Mail()
 moment = Moment()
