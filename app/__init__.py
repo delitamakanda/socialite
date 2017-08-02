b57e7f2e7154152693a25e835a6f591fde882d14
Délita Makanda <delita.makanda@gmail.com>
2017-08-02 21:18:43 +0200

    fix flask assets putain de toi github

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from flask_assets import Bundle, Environment
 from flask.ext.mail import Mail
 from flask.ext.login import LoginManager
 from flask.ext.moment import Moment
@@ -6,8 +7,6 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.pagedown import PageDown
 from flask.ext.flatpages import FlatPages
 from config import config
-
-from util import assets
 
 mail = Mail()
 moment = Moment()
@@ -21,6 +20,7 @@ login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
     app = Flask(__name__)
+    assets = Environment(app)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
 
@@ -41,5 +41,20 @@ def create_app(config_name):
     app.register_blueprint(auth_blueprint, url_prefix='/auth')
     app.register_blueprint(main_blueprint)
     app.register_blueprint(api_1_0_blueprint, url_prefix='/api/v1.0')
+
+    bundles = {
+      'root_js': Bundle(
+        'js/lib/jquery-2.1.1.min.js',
+        'js/app.js',
+        output='dist/bundle.js'),
+
+      'root_css': Bundle(
+        'css/lib/reset.css',
+        'css/common.css',
+        'css/app.css',
+        output='dist/styles.css')
+    }
+
+    assets.register(bundles)
 
     return app
