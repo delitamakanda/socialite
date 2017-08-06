0850ea07f07360540e5ae3343854ba531006e424
Délita Makanda <delita.makanda@gmail.com>
2017-08-06 12:39:09 +0200

    feature alert notification

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,1 +1,16 @@
+'use strict';
 
+$(function() {
+    var close = $('.close-btn');
+    var i;
+
+    for (i = 0; i < close.length; i++) {
+        close.on('click', function(){
+            var div = $(this).parent();
+            div.css('opacity', 0);
+            setTimeout(function() {
+                div.css('display', 'none');
+            }, 600);
+        })
+    }
+});
