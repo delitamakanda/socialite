eaf722b87f60d79a54cd0024553bede37ce7709e
Délita Makanda <delitamakanda@users.noreply.github.com>
2017-08-07 13:15:02 +0200

    fix mail_sender error 501

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
 	SECRET_KEY = os.environ.get('SECRET_KEY')
 	MAIL_SUBJECT_PREFIX = 'socialite app'
-	MAIL_SENDER = 'Socialite Team'
+	MAIL_SENDER = 'Socialite Team <postmaster@sandbox928442dcb1314427b37d82a94f20f54a.mailgun.org>'
 	ADMIN = ['delita.makanda@gmail.com']
 	POSTS_PER_PAGE = 10
 	FOLLOWERS_PER_PAGE = 20
