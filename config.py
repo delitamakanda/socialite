4e7f85684304630b9bf18b10919e564034bfa98c
Délita Makanda <delitamakanda@users.noreply.github.com>
2017-08-08 15:38:14 +0200

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
 	SECRET_KEY = os.environ.get('SECRET_KEY')
 	MAIL_SUBJECT_PREFIX = 'socialite app'
-	MAIL_SENDER = 'Socialite Team <postmaster@sandbox928442dcb1314427b37d82a94f20f54a.mailgun.org>'
+	MAIL_SENDER = os.environ.get('MAIL_USERNAME')
 	ADMIN = ['delita.makanda@gmail.com']
 	POSTS_PER_PAGE = 10
 	FOLLOWERS_PER_PAGE = 20
