77b73f9def931bb33be73e5fb6d7d2f88913e7a0
Délita Makanda <delitamakanda@users.noreply.github.com>
2017-08-16 16:10:47 +0200

    Update __init__.py

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -2,7 +2,6 @@ from flask import Blueprint
 main = Blueprint('main', __name__)
 from . import views, errors
 from ..models import Permission
-from . import cache
 
 @main.app_context_processor
 def inject_permissions():
