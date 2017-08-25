88e71eb51e255266b3e9efa7a14dd3b00292abb2
Délita Makanda <delitamakanda@users.noreply.github.com>
2017-08-25 16:09:59 +0200

    Update views.py

--- app/main/views.py
+++ app/main/views.py
@@ -281,12 +281,10 @@ def sitemap():
     
     for rule in current_app.url_map.iter_rules():
         if "GET" in rule.methods and len(rule.arguments)==0:
-            pages.append(
-                [rule.rule,ten_days_ago]
-            )
+            pages.append([rule.rule,ten_days_ago])
 
     sitemap_xml = render_template('sitemap_template.xml', pages=pages)
-    response= make_response(sitemap_xml)
+    response = make_response(sitemap_xml)
     response.headers["Content-Type"] = "application/xml"
     
     return response
