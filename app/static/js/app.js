ce6d94599e6c0794dc9079a83f558da9420482b0
Délita Makanda <delitamakanda@users.noreply.github.com>
2017-09-07 11:28:56 +0200

    add socket io

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -14,3 +14,8 @@ $(function() {
         })
     }
 });
+
+var socket = io.connect('http://' + document.domain + ':' + location.port);
+socket.on('connect', function() {
+    socket.emit('my event', {data: 'I\'m connected!'});
+});
